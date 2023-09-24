e95a2e48676a0e7de3d77e21bb642181c8ca7fc2
Eric <76673343+ericfayhuynh@users.noreply.github.com>
2023-09-24 15:39:58 -0700

    Update script.js

--- eric_test/frontend/static/script.js
+++ eric_test/frontend/static/script.js
@@ -1,6 +1,6 @@
 // Map
 const map = L.map('map').setView([40.738873, -73.959579], 13); // Coordinates moved a bit more west in NYC
-const url = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJpY2ZheWh1eW5oIiwiYSI6ImNsbXBzYmJteTBoNXoybG51bnlwaXNxaDQifQ.fQ6iaJMaFDwYlxWRMvuFBA`;
+const url = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=API_KEY`;
 
 L.tileLayer(url, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
